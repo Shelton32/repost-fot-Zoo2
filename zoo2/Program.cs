1b90543ad481fbd6838ff237c88aae10501b7112
Shelton32 <iv.mihal190170@gmail.com>
2025-02-04 18:42:30 +0300

    yes

--- zoo2/Program.cs
+++ zoo2/Program.cs
@@ -2,3 +2,8 @@
 
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, World!");
